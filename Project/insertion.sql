a62f00d66b5c30e57241207d48967ef38c8feda1
jestmine <162778881+jestmine@users.noreply.github.com>
2024-11-11 23:49:30 -0800

    Update insertion.sql

--- Project/insertion.sql
+++ Project/insertion.sql
@@ -9,20 +9,20 @@ INSERT INTO Restaurant (RestaurantID, Name, Address, City, State, ZipCode, Websi
 (7, 'Kimchi Pot', '516 Goodwill St', 'Milpitas', 'CA', '95035', 'http://kimchipot.com', 5.0, 'Korean'),
 (8, 'Cajun Kitchen', '718 Nolin St', 'Miami', 'FL', '68630', 'http://cajunkitchen.com', 4.2, 'American'),
 (9, 'Chicken n' Go', '920 Amelia St', 'Seattle', 'WA', '45627', 'http://chickenngo.com', 4.7, 'American'),
-(10, 'Pho Palet', '212 Sheets St', 'Las Vegas', 'NV', '57383', 'http://phopalet.com', 2.3, 'Vietnamese');
+(10, 'Pho Palate', '212 Sheets St', 'Las Vegas', 'NV', '57383', 'http://phopalet.com', 2.3, 'Vietnamese');
 
 
 INSERT INTO Orders (OrderID, UserID, OrderDate, OrderTotal, DeliveryAddress) VALUES
 (1, 1, '2024-10-20', 45.00, '123 Main St, San Francisco'),
 (2, 2, '2024-10-21', 60.00, '456 Elm St, New York')
-(3, 3, '2024-10-19', 60.00, '789 Neem St, Madison')
-(4, 4, '2024-10-23', 60.00, '101 Juana St, Reno')
-(5, 5, '2024-10-22', 60.00, '112 Wisto St, Sacremento')
-(6, 6, '2024-10-25', 60.00, '134 Arizona St, Austin')
+(3, 3, '2024-10-19', 90.00, '789 Neem St, Madison')
+(4, 4, '2024-10-23', 30.00, '101 Juana St, Reno')
+(5, 5, '2024-10-22', 40.00, '112 Wisto St, Sacremento')
+(6, 6, '2024-10-25', 50.00, '134 Arizona St, Austin')
 (7, 7, '2024-10-26', 60.00, '516 Goodwill St, Milpitas')
-(8, 8, '2024-10-26', 60.00, '718 Nolin St, Miami')
-(9, 9, '2024-10-28', 60.00, '920 Amelia St, Seattle')
-(10, 10, '2024-10-31', 60.00, '212 Sheets St, Las Vegas');
+(8, 8, '2024-10-26', 75.00, '718 Nolin St, Miami')
+(9, 9, '2024-10-28', 30.00, '920 Amelia St, Seattle')
+(10, 10, '2024-10-31', 80.00, '212 Sheets St, Las Vegas');
 
 INSERT INTO DishOrder (DishOrderID, OrderID, DishID, Quantity) VALUES
 (1, 1, 1, 2),
@@ -61,15 +61,39 @@ INSERT INTO Favorites (FavoriteID, UserID, RestaurantID) VALUES
 
 INSERT INTO Reservation (ReservationID, UserID, RestaurantID, ReservationDate, ReservationTime, NumberOfGuests, SpecialRequests) VALUES
 (1, 1, 1, '2024-11-15', '18:00:00', 4, 'Window seat'),
-(2, 2, 2, '2024-11-16', '19:00:00', 2, 'Quiet area');
+(2, 2, 2, '2024-11-16', '19:00:00', 2, 'Quiet area'),
+(3, 3, 3, '2024-11-15', '18:00:00', 7, 'Big table'),
+(4, 4, 4, '2024-11-16', '19:00:00', 2, 'Corner table'),
+(5, 5, 5, '2024-11-15', '18:00:00', 4, 'Window seat'),
+(6, 6, 6, '2024-11-16', '19:00:00', 2, 'Private area'),
+(7, 7, 7, '2024-11-15', '18:00:00', 4, 'Booth'),
+(8, 8, 8, '2024-11-16', '19:00:00', 2, 'Quiet area'),
+(9, 9, 9, '2024-11-15', '18:00:00', 4, 'Window seat'),
+(10, 10, 10, '2024-11-16', '19:00:00', 2, 'Quiet area');
 
 INSERT INTO Menu (MenuID, RestaurantID, MenuName, Description) VALUES
 (1, 1, 'Dinner Menu', 'Our finest selection of Italian cuisine'),
-(2, 2, 'Lunch Menu', 'Delicious Japanese specialties');
+(2, 2, 'Lunch Menu', 'Delicious Japanese specialties'),
+(3, 3, 'Dinner Menu', 'Authentic Indian selections),
+(4, 4, 'Lunch Menu', 'Best tacos compilations'),
+(5, 5, 'Dinner Menu', 'Flavorful blast of Shawarmas'),
+(6, 6, 'Lunch Menu', 'Delicious Pizzas'),
+(7, 7, 'Dinner Menu', 'Korean delicacies'),
+(8, 8, 'Lunch Menu', 'Spiciest and mouthwatering specials'),
+(9, 9, 'Dinner Menu', 'Chickens of all kinds'),
+(10, 10, 'Lunch Menu', 'Treat for your palate');
 
 INSERT INTO Dish (DishID, MenuID, DishName, Description, Price) VALUES
 (1, 1, 'Spaghetti Carbonara', 'Classic Italian pasta dish', 15.00),
-(2, 2, 'Salmon Sushi', 'Fresh salmon on rice', 12.00);
+(2, 2, 'Salmon Sushi', 'Fresh salmon on rice', 12.00),
+(3, 3, 'Butter Chicken and Naan', 'Classic Indian cuisine', 20.00),
+(4, 4, 'Beef Tacos', 'Ten Beef Tacos served with cilantro and onions', 10.00),
+(5, 5, 'Falafel', 'Classic Meditterean wrap with chicken', 18.00),
+(6, 6, 'Meat Lovers', 'Three meat pizza with extra cheese', 20.00),
+(7, 7, 'Carbonara Ttebokki', 'Classic Korean rice cake dish', 25.00),
+(8, 8, 'Smoked Spicy Crab', 'Fresh crab marinated in cajun spices', 27.00),
+(9, 9, 'Garlic Cheese Chicken', 'Fried Chicken with garlic seasoning and topped with cheese', 10.00),
+(10, 10, 'Classic Pho', 'Chef's special pho with choice of meat', 30.00);
 
 -- INSERT INTO Review (ReviewID, UserID, RestaurantID, Rating, Comment, DatePosted) VALUES
 -- (3, 1, 1, 4.5, 'WOW FOOD IG!', '2024-11-10');
